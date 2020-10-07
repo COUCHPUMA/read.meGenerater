d8b6a59d739fa6473505f9a68dfb3c94d98068ec
Richard Katsnelson <rkatsnelson91@gmail.com>
2020-10-06 21:39:21 -0400

    typed out questions

--- index.js
+++ index.js
@@ -7,4 +7,64 @@ const writeFileAsync = util.promisify(fs.writeFile);
 
 //Prompt the user questions to populate the README.md
 function promptUser() {
-    return inquirer.prompt([
+    return inquirer.prompt([{
+            type: "input",
+            Nname: "projectTitle",
+            message: "What is your Project's Title?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "write a brief description of your Project:"
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Describe the installation process if any: ",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "What is this project usage for?"
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Chose the appropriate license for this project: ",
+            choices: [
+                "Apache",
+                "Academic",
+                "GNU",
+                "ISC",
+                "MIT",
+                "Mozilla",
+                "Open"
+            ]
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "Who are the contributors of this projects?"
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Is there a test included?"
+        },
+        {
+            type: "input",
+            name: "questions",
+            message: "What do I do if I have an issue? "
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "Please enter your GitHub username: "
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email: "
+        }
+    ]);
+}
